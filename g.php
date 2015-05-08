e4b13e36b79825996615d8cb34ab9d9a05c97fb2
betkapolymorphic <a_albert@ukr.net>
2015-05-09 01:18:29 +0300

    fix g.php cyrillic symbols

--- g.php
+++ g.php
@@ -1,5 +1,38 @@
 <?php
-    include_once 'storageController.php';
+
+    function pathinfo_utf($path) {
+
+        if (strpos($path, '/') !== false)
+            $basename = end(explode('/', $path));
+        elseif (strpos($path, '\\') !== false)
+            $basename = end(explode('\\', $path));
+        else
+            return false;
+
+        if (!$basename)
+            return false;
+
+        $dirname = substr($path, 0,
+            strlen($path) - strlen($basename) - 1);
+
+        if (strpos($basename, '.') !== false) {
+            $extension = end(explode('.', $path));
+            $filename = substr($basename, 0,
+                strlen($basename) - strlen($extension) - 1);
+        } else {
+            $extension = '';
+            $filename = $basename;
+        }
+
+        return array (
+            'dirname' => $dirname,
+            'basename' => $basename,
+            'extension' => $extension,
+            'filename' => $filename
+        );
+    }
+
+include_once 'storageController.php';
 
     if(!isset($_GET['l'])){
         die("Error : empty link");
@@ -11,7 +44,7 @@
 
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename='.basename($file));
+        header('Content-Disposition: attachment; filename='.pathinfo_utf($file)['basename']);
         header('Content-Transfer-Encoding: binary');
         header('Expires: 0');
         header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
